a06ea6a570dd5323acaa99cb8a1a4dd886044455
SagarSurfer <vidhyasagar412@gmail.com>
2020-06-15 00:11:17 +0530

    "Converted the large JSON files"

--- data/topic_dict/python_conv.py
+++ data/topic_dict/python_conv.py
@@ -1,14 +1,14 @@
 import csv, json
 
-csvFilePath = "original_hm.csv"
-jsonFilePath = "original_hm.json"
+csvFilePath = "pets-dict.csv"
+jsonFilePath = "pets-dict.json"
 
 data ={}
 
 with open(csvFilePath) as csvFile:
     csvReader = csv.DictReader(csvFile)
     for csvRow in csvReader:
-        hmid = csvRow["hmid"]
-    data[hmid] = csvRow
+        CEO = csvRow["CEO"]
+    data[CEO] = csvRow
 with open(jsonFilePath, "w") as jsonFile:
-    jsonFile.write(jsonFile.dumps(data, indent= 4))
+    jsonFile.write(jsonFile.dumps())
